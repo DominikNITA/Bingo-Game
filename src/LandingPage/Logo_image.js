7bc59490b36b79c4bc8669ec3d16f4ce0105b5b3
dominik.nita <dominik.nita@gmail.com>
2019-04-15 23:48:54 +0200

    a

---
+++ src/LandingPage/Logo_image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo_image = () => {
+    return (
+        <div>logo</div>
+    )
+}
+
+export default Logo_image;
---
+++ src/LandingPage/Logo_text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo_text = () => {
+    return (
+        <div>
+            <div className="firstLogoText">Cthulhu</div>
+            <div className="secondLogoText">Bingo</div>
+        </div>
+    )
+}
+
+export default Logo_text;
