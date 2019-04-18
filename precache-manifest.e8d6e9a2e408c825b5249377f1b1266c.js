e2ec67a339059efb5ca3c64a672d041b4a10d330
dominik.nita <dominik.nita@gmail.com>
2019-04-18 02:08:08 +0200

    Updates

---
+++ precache-manifest.e8d6e9a2e408c825b5249377f1b1266c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "66f0f4bdd5b6f47d145bbc4e2bce4b9b",
+    "url": "/Bingo-Game/static/media/logo.66f0f4bd.svg"
+  },
+  {
+    "revision": "92f68ee35f5c60fc8778fb959347ce0b",
+    "url": "/Bingo-Game/static/media/latest.92f68ee3.svg"
+  },
+  {
+    "revision": "afc6b30e93950027e5853ffcfb1495bb",
+    "url": "/Bingo-Game/static/media/bulletHole.afc6b30e.svg"
+  },
+  {
+    "revision": "4dc7fbbe2caaddeaadd3",
+    "url": "/Bingo-Game/static/js/runtime~main.701e1beb.js"
+  },
+  {
+    "revision": "fa9af3cb63b2406f198d",
+    "url": "/Bingo-Game/static/js/main.e2d3c5d9.chunk.js"
+  },
+  {
+    "revision": "72b81a29858e43b094ba",
+    "url": "/Bingo-Game/static/js/2.2d90e0d3.chunk.js"
+  },
+  {
+    "revision": "fa9af3cb63b2406f198d",
+    "url": "/Bingo-Game/static/css/main.0a8a6000.chunk.css"
+  },
+  {
+    "revision": "7dc16715e653a55cee83e25d38325266",
+    "url": "/Bingo-Game/index.html"
+  }
+];
